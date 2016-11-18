13852fdbd826b830b0293dd377a76849d6831800
Austin Schwartz <schwar12@purdue.edu>
2016-11-18 12:27:29 -0500

    Randomly places ships + attacks

--- StarterCode/Battleship$Test.scala
+++ StarterCode/Battleship$Test.scala
@@ -25,4 +25,39 @@ class Battleship$Test extends org.scalatest.FunSuite {
     assert(Battleship.lettersToRC("B7") == (1, 7))
     assert(Battleship.lettersToRC("H7") == (7, 7))
   }
+
+  test("ship is valid") {
+    val ship = new Battleship.Ship("fuck", 2, 0, 0, Battleship.Right)
+    assert(ship.isValid(0, 0, Battleship.Right, 2))
+    assert(ship.isValid(0, 1, Battleship.Right, 2))
+    assert(ship.isValid(0, 2, Battleship.Right, 2))
+    assert(ship.isValid(0, 3, Battleship.Right, 2))
+    assert(ship.isValid(0, 4, Battleship.Right, 2))
+    assert(ship.isValid(0, 5, Battleship.Right, 2))
+    assert(ship.isValid(0, 6, Battleship.Right, 2))
+    assert(!ship.isValid(0, 7, Battleship.Right, 2))
+    assert(ship.isValid(0, 7, Battleship.Down, 2))
+    assert(!ship.isValid(0, 7, Battleship.Up, 2))
+    assert(ship.isValid(1, 7, Battleship.Up, 2))
+    assert(!ship.isValid(1, 7, Battleship.Up, 3))
+    assert(ship.isValid(7, 7, Battleship.Up, 6))
+    assert(ship.isValid(7, 7, Battleship.Left, 6))
+    assert(ship.isValid(7, 7, Battleship.Right, 1))
+    assert(ship.isValid(7, 7, Battleship.Down, 1))
+    assert(!ship.isValid(7, 7, Battleship.Right, 2))
+    assert(!ship.isValid(7, 7, Battleship.Down, 2))
+    assert(ship.isValid(3, 3, Battleship.Down, 3))
+    assert(ship.isValid(3, 3, Battleship.Up, 3))
+    assert(ship.isValid(2, 7, Battleship.Down, 5))
+  }
+
+  test("flip works") {
+    assert(Battleship.flip(("G3","G7")) == ("G3", "G7"))
+    assert(Battleship.flip(("G7","G3")) == ("G3", "G7"))
+    assert(Battleship.flip(("A7","B7")) == ("A7", "B7"))
+    assert(Battleship.flip(("B7","A7")) == ("A7", "B7"))
+    assert(Battleship.flip(("A0","A1")) == ("A0", "A1"))
+    assert(Battleship.flip(("A1","A0")) == ("A0", "A1"))
+  }
+
 }
--- StarterCode/Battleship.scala
+++ StarterCode/Battleship.scala
@@ -13,27 +13,94 @@ import java.net.InetAddress
 import java.lang.Thread
 
 import scala.collection.mutable
+import scala.util.control.Breaks
 
 object Battleship {
-  class Ship(var name: String, var length: Int, var startPos: (String, String));
+  class Ship(
+    var name: String,
+    var length: Int,
+    var i: Int,
+    var j: Int,
+    var dir: Direction
+  ) {
+    var startPos = getPos(i, j, dir, length)
+
+    def getPos(r: Int, c: Int, direc: Direction, len: Int): (String, String) = {
+      flip(
+        rcToLetters(r, c),
+        direc match {
+          case Right => rcToLetters(r, c + len - 1)
+          case Left  => rcToLetters(r, c - len + 1)
+          case Down  => rcToLetters(r + len - 1, c)
+          case Up    => rcToLetters(r - len + 1, c)
+          case _ => "A0"
+        }
+      )
+    }
+
+    def isValid(r: Int, c: Int, direc: Direction, len: Int): Boolean = {
+      return r >= 0 && c >= 0 && r < DIM && c < DIM && (direc match {
+        case Right => c + len - 1 < DIM
+        case Left => c - len + 1 >= 0
+        case Down => r + len - 1 < DIM
+        case Up => r - len + 1 >= 0
+      })
+      false
+    }
+  }
 
   def main(args: Array[String]) {
-    while (true) {
-      this.connectToServer
-      if (this.socket != null)
-        gameMain
+    var fuckScala = new Breaks
+    var flag = false
+    if (!flag) {
+      fuckScala.breakable {
+        while (true) {
+          try {
+            this.connectToServer
+            if (this.socket != null)
+              gameMain
+          } catch {
+            case e: Exception => {
+              println(e.toString())
+              flag = true
+              fuckScala.break()
+            }
+          }
+        }
+      }
     }
-//    randomizeShips(List(2, 3, 3, 4, 5))
+//    placeShips("SHWR")
 //    ships.map((x: Ship) => {
 //      val i: (String, String) = x.startPos
 //      println("<" + i._1 + " " + i._2 + ">")
 //    })
-//    placeShips("SHWR")
 //    printBoard(this.mygrid)
   }
 
+  def flip(letters: (String, String)): (String, String) = {
+    val (startX, startY) = lettersToRC(letters._1)
+    val (endX, endY) = lettersToRC(letters._2)
+    if (startX == endX) {
+      if (endY < startY) {
+        return (letters._2, letters._1)
+      }
+    } else if (startY == endY) {
+      if (startX > endX) {
+        return (letters._2, letters._1)
+      }
+    }
+    return letters
+  }
+
+  sealed trait Direction
+  final case object Left extends Direction
+  final case object Right extends Direction
+  final case object Up extends Direction
+  final case object Down extends Direction
+  var directions: List[Direction] = List(Left, Right, Up, Down)
+
   val DIM = 8
-  var DEBUG = false
+  var DEBUG = true
 
   val rand = new scala.util.Random
 
@@ -47,11 +114,11 @@ object Battleship {
   def rcToLetters(r: Int, c: Int): String = letters(r) + "" + String.valueOf(c)
   def lettersToRC(str: String): (Int, Int) = (letters.indexOf(str(0)), Integer.valueOf(str(1)) - '0')
 
-  var destroyer = new Ship("destroyer", 2, ("A0", "A1")) // 2
-  var submarine = new Ship("destroyer", 3, ("B0", "B2")) // 3
-  var cruiser = new Ship("cruiser", 3, ("C0", "C2")) // 3
-  var battleship = new Ship("battleship", 4, ("D0", "D3")) // 4
-  var carrier = new Ship("carrier", 5, ("E0", "E4")) // 5
+  var destroyer = new Ship("destroyer", 2, 0, 0, Right) // 2
+  var submarine = new Ship("submarine", 3, 1, 0, Right) // 3
+  var cruiser = new Ship("cruiser", 3, 2, 0, Right) // 3
+  var battleship = new Ship("battleship", 4, 3, 0, Right) // 4
+  var carrier = new Ship("carrier", 5, 4, 0, Right) // 5
   var ships = List[Ship](destroyer, submarine, cruiser, battleship, carrier)
   var shipMap = Map(
     "destroyer" -> destroyer,
@@ -61,18 +128,28 @@ object Battleship {
     "carrier" -> carrier
   )
 
-  def isValidPlacement(rs: (Int, Int), cs: (Int, Int)): Boolean = {
-    (rs._1 to rs._2).foreach((r: Int) => {
-      (cs._1 to cs._2).foreach((c: Int) => {
+  def isValidPlacement(rs: String, cs: String): Boolean = isValidPlacement(lettersToRC(rs), lettersToRC(cs))
+  def isValidPlacement(start: (Int, Int), end: (Int, Int)): Boolean = {
+    var lowerR = Math.min(start._1, end._1)
+    var higherR = Math.max(start._1, end._1)
+    var lowerC = Math.min(start._2, end._2)
+    var higherC = Math.max(start._2, end._2)
+    (lowerR to higherR).foreach((r: Int) => {
+      (lowerC to higherC).foreach((c: Int) => {
         if (mygrid(r)(c) != -1)
           return false
       })
     })
-    true
+    return true
   }
 
   def printBoard(g: Array[Array[Int]]): Unit = {
+    print(' ')
+    letters.foreach((i: Char) => print(i))
+    println()
+    var num = 0
     g.foreach((row: Array[Int]) => {
+      print(num)
       row.foreach((u: Int) => {
         if (u == -1)
           print(' ')
@@ -80,23 +157,39 @@ object Battleship {
           print('o')
       })
       println()
+      num = num + 1
     })
   }
-
-  def isValidPlacement(rs: String, cs: String): Boolean = isValidPlacement(lettersToRC(rs), lettersToRC(cs))
 
   def placeShipRandom(ship: Ship): Unit = {
     var placed: Boolean = false
     while (!placed) {
-      var row = rand.nextInt(DIM)
-      var col = rand.nextInt(DIM)
-      placed = true
+      val row = rand.nextInt(DIM)
+      val col = rand.nextInt(DIM)
+      var i = 0
+      while (i < 6 && !placed) {
+        val dir = directions(rand.nextInt(directions.length))
+
+        if (ship.isValid(row, col, dir, ship.length) &&
+          isValidPlacement(
+            flip(ship.getPos(row, col, dir, ship.length))._1,
+            flip(ship.getPos(row, col, dir, ship.length))._2
+          )) {
+          ship.i = row
+          ship.j = col
+          ship.startPos = flip(ship.getPos(row, col, dir, ship.length))
+          ship.dir = dir
+          placed = true
+        }
+        i = i + 1
+      }
     }
   }
 
-  def randomizeShips(lengths: List[Int]): Unit =  {
+  def randomizeShips(): Unit =  {
     ships.reverse.foreach((ship: Ship) => {
       placeShipRandom(ship)
+      updateMyGrid(ship)
     })
   }
 
@@ -110,15 +203,28 @@ object Battleship {
       }
     }
 
-    //randomizeShips(ships)
-    ships.foreach((ship: Ship) => {
-      var start: (Int, Int) = lettersToRC(ship.startPos._1)
-      var end: (Int, Int) = lettersToRC(ship.startPos._2)
-      (start._1 to end._1).foreach((r: Int) => {
-        (start._2 to end._2).foreach((c: Int) => {
-          mygrid(r)(c) = 1
-        })
+    randomizeShips()
+    printBoard(mygrid)
+  }
+
+  def updateMyGrid(ship: Ship): Unit = {
+    var start: (Int, Int) = lettersToRC(ship.startPos._1)
+    var end: (Int, Int) = lettersToRC(ship.startPos._2)
+    println(ship.name, ship.length, ship.startPos, start, end)
+    var lowerR = Math.min(start._1, end._1)
+    var higherR = Math.max(start._1, end._1)
+    var lowerC = Math.min(start._2, end._2)
+    var higherC = Math.max(start._2, end._2)
+    (lowerR to higherR).foreach((r: Int) => {
+      (lowerC to higherC).foreach((c: Int) => {
+        //println((r, c))
+        mygrid(r)(c) = 1 // wtf?
       })
+    })
+  }
+  def updateMyGrid(): Unit = {
+    ships.foreach((ship: Ship) => {
+      updateMyGrid(ship)
     })
   }
 
@@ -208,26 +314,31 @@ object Battleship {
           }
         }
         else if (data.contains("Destroyer")) {
+          println(destroyer.startPos)
           this.out.print(destroyer.startPos._1)
           this.out.print(destroyer.startPos._2)
           out.flush
         }
         else if (data.contains("Submarine")) {
+          println(submarine.startPos)
           this.out.print(submarine.startPos._1)
           this.out.print(submarine.startPos._2)
           out.flush
         }
         else if (data.contains("Cruiser")) {
+          println(cruiser.startPos)
           this.out.print(cruiser.startPos._1)
           this.out.print(cruiser.startPos._2)
           out.flush
         }
         else if (data.contains("Battleship")) {
+          println(battleship.startPos)
           this.out.print(battleship.startPos._1)
           this.out.print(battleship.startPos._2)
           out.flush
         }
         else if (data.contains("Carrier")) {
+          println(carrier.startPos)
           this.out.print(carrier.startPos._1)
           this.out.print(carrier.startPos._2)
           out.flush
